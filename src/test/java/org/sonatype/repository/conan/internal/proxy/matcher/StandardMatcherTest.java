14d585fca1329e311c9323b0b5871be99aeec9c8
Mark Dodgson <mdodgson@sonatype.com>
2018-03-22 07:31:26 +0000

    Finish matcher tests

--- src/test/java/org/sonatype/repository/conan/internal/proxy/matcher/StandardMatcherTest.java
+++ src/test/java/org/sonatype/repository/conan/internal/proxy/matcher/StandardMatcherTest.java
@@ -6,7 +6,6 @@ import org.sonatype.nexus.repository.http.HttpMethods;
 import org.sonatype.nexus.repository.view.Context;
 import org.sonatype.nexus.repository.view.Handler;
 import org.sonatype.nexus.repository.view.Request;
-import org.sonatype.nexus.repository.view.Route;
 import org.sonatype.nexus.repository.view.matchers.token.TokenMatcher;
 
 import org.junit.Before;
@@ -49,9 +48,58 @@ public class StandardMatcherTest
 
   @Test
   public void canMatchOnDownloadUrls() {
-    when(request.getPath()).thenReturn("v1/conans/Hello/0.2/demo/testing/download_urls");
-    Route route = underTest.downloadUrls().handler(handler).create();
-    assertTrue(route.getMatcher().matches(context));
+    when(request.getPath()).thenReturn("/v1/conans/Hello/0.2/demo/testing/download_urls");
+    assertTrue(underTest.downloadUrls().handler(handler).create().getMatcher().matches(context));
+    TokenMatcher.State matcherState = attributesMap.require(TokenMatcher.State.class);
+    assertThat(matcherState.getTokens().get("group"), is(equalTo("demo")));
+    assertThat(matcherState.getTokens().get("project"), is(equalTo("Hello")));
+    assertThat(matcherState.getTokens().get("version"), is(equalTo("0.2")));
+  }
+
+  @Test
+  public void canMatchOnPackagesDownloadUrl() {
+    when(request.getPath()).thenReturn("/v1/conans/Hello/0.2/demo/testing/packages/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/download_urls");
+    assertTrue(underTest.downloadUrls().handler(handler).create().getMatcher().matches(context));
+    TokenMatcher.State matcherState = attributesMap.require(TokenMatcher.State.class);
+    assertThat(matcherState.getTokens().get("group"), is(equalTo("demo")));
+    assertThat(matcherState.getTokens().get("project"), is(equalTo("Hello")));
+    assertThat(matcherState.getTokens().get("version"), is(equalTo("0.2")));
+  }
+
+  @Test
+  public void canMatchOnConanfile() {
+    when(request.getPath()).thenReturn("/v1/files/Hello/0.2/demo/testing/export/conanfile.py");
+    assertTrue(underTest.conanFile().handler(handler).create().getMatcher().matches(context));
+    TokenMatcher.State matcherState = attributesMap.require(TokenMatcher.State.class);
+    assertThat(matcherState.getTokens().get("group"), is(equalTo("demo")));
+    assertThat(matcherState.getTokens().get("project"), is(equalTo("Hello")));
+    assertThat(matcherState.getTokens().get("version"), is(equalTo("0.2")));
+  }
+
+  @Test
+  public void canMatchOnConanManifest() {
+    when(request.getPath()).thenReturn("/v1/files/Hello/0.2/demo/testing/export/conanmanifest.txt");
+    assertTrue(underTest.conanManifest().handler(handler).create().getMatcher().matches(context));
+    TokenMatcher.State matcherState = attributesMap.require(TokenMatcher.State.class);
+    assertThat(matcherState.getTokens().get("group"), is(equalTo("demo")));
+    assertThat(matcherState.getTokens().get("project"), is(equalTo("Hello")));
+    assertThat(matcherState.getTokens().get("version"), is(equalTo("0.2")));
+  }
+
+  @Test
+  public void canMatchOnConanInfo() {
+    when(request.getPath()).thenReturn("/v1/files/Hello/0.2/demo/testing/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/conaninfo.txt");
+    assertTrue(underTest.conanInfo().handler(handler).create().getMatcher().matches(context));
+    TokenMatcher.State matcherState = attributesMap.require(TokenMatcher.State.class);
+    assertThat(matcherState.getTokens().get("group"), is(equalTo("demo")));
+    assertThat(matcherState.getTokens().get("project"), is(equalTo("Hello")));
+    assertThat(matcherState.getTokens().get("version"), is(equalTo("0.2")));
+  }
+
+  @Test
+  public void canMatchOnConanPackage() {
+    when(request.getPath()).thenReturn("/v1/files/Hello/0.2/demo/testing/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/conan_package.tgz");
+    assertTrue(underTest.conanPackage().handler(handler).create().getMatcher().matches(context));
     TokenMatcher.State matcherState = attributesMap.require(TokenMatcher.State.class);
     assertThat(matcherState.getTokens().get("group"), is(equalTo("demo")));
     assertThat(matcherState.getTokens().get("project"), is(equalTo("Hello")));
